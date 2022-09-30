44ba7ecc3de5d72f398e25fd2b2efefb5029c07c
Alejandro Vega <alejandro.vega@claritymis.com>
2022-09-29 21:45:49 -0500

    Added Orm and changed to use credentials from .env file

--- packages/api/src/app/app.module.ts
+++ packages/api/src/app/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_TYPE,
+      entities: [],
+      synchronize: process.env.ENV === 'DEV',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
